dfb740a2a00ce4e923e2ccc17fa2768e1baf2c49
Colin Baxter <cwbaxter@uw.edu>
2023-11-30 22:12:18 -0800

    Rearranged file structure, setup temporal data analysis so it works with .csvs in github Added feature to select either hourly or daily data.

--- codebase/src/pages/2_Temporal_Data.py
+++ codebase/src/pages/2_Temporal_Data.py
@@ -13,40 +13,65 @@ import pandas as pd
 import plotly.express as px
 import numpy as np
 
-dirname = os.path.dirname(__file__) #Show cwd, should be ~\FrontEnd
-testData = os.path.join(dirname, 'Merged_1127.csv') # CSV needs to have this name
-                                                # CSV will eventually be replaced by
-                                                # API dataframe
+def df_creation(dir, frequency):
+    st.subheader("Select data locations")
+    #Static list of data collection sites
+    LOCATIONS = ["Beach2_Buoy", "Beach2_Tower", "Beach6_Buoy", "TREC_Tower"]
+    # Checkboxes for user to select collection sites
+    locations_selected = st.multiselect(
+        "Data collection sites",
+        LOCATIONS
+    )
+    # List comprehension to create list of paths to needed dataframes
+    df_dirs = [(dir + "/data/iChart6_data/processed/" + loc + frequency) for loc in locations_selected]
+    # Concatenate all of the dfs into one
+    df_chosen_locs = pd.concat(map(pd.read_csv, df_dirs))
+    return df_chosen_locs
 
-df = pd.read_csv(testData)
-df['Time'] = pd.to_datetime(df['Time'])
+
+dirname = os.path.dirname(__file__) #Relative path to /pages
 st.set_page_config(layout="wide") # Page configuration must be first Streamlit command called
 st.title("Historical Buoy Data") # Page title
 
-LOCATIONS = list(df['Location'].unique()) # List of all weather station locations
-VARIABLES = list(df['variable'].unique()) # List of all variable types
+#Hourly or daily choice - default is daily
+frequency_selected = st.radio(
+    "Select data interval",
+    ["Hourly", "Daily"],
+    index=0
+)
+
+if frequency_selected == "Daily":
+    df = df_creation(dirname, "/daily_tidy_all_data.csv")
+else:
+    df = df_creation(dirname, "/hourly_tidy_all_data.csv")
+
+#Convert time column to datetime
+df['times'] = pd.to_datetime(df['times'])
+
+locations_in_df = list(df['location'].unique()) # List of all weather station locations
+variables_in_df = list(df['parameter'].unique()) # List of all variable types
 
 with st.sidebar: # sidebar hides the drop-down
     st.subheader("Configure data selection")
-    START_DATE = st.date_input("Choose start-date", min_value=datetime.date(2021, 7, 21), max_value=datetime.date(2023, 8, 1), value = None)
+    START_DATE = st.date_input("Choose start-date", min_value=datetime.date(2008, 1, 1), max_value=datetime.date(2023, 9, 1), value = None)
     END_DATE = st.date_input("Choose end-date", value = None)
 
-    LOCATION = st.selectbox(label = "Choose a location", options = LOCATIONS)
+    LOCATION = st.selectbox(label = "Choose a location", options = locations_in_df)
     #Figure out how to make this multiselect work
     #LOCATION = st.multiselect('Choose desired locations', LOCATIONS)
-    VARIABLE = st.selectbox(label = "Choose a variable", options = VARIABLES)
+    VARIABLE = st.selectbox(label = "Choose a variable", options = variables_in_df)
 
 #Create temporary dataframe based on date range
-mask = (df['Time'] > np.datetime64(START_DATE)) & (df['Time'] <= np.datetime64(END_DATE))
+mask = (df['times'] > np.datetime64(START_DATE)) & (df['times'] <= np.datetime64(END_DATE))
 df_intime = df.loc[mask]
 
 # Configure temporary dataframe to queried values
-df_viz = df_intime.query(f"Location=='{LOCATION}'").query(f"variable=='{VARIABLE}'")
+df_viz = df_intime.query(f"location=='{LOCATION}'").query(f"parameter=='{VARIABLE}'")
 
 # Title of plot
 GRAPH_TITLE = f"{VARIABLE} over Time for {LOCATION}"
 #Create plot figure
-fig = px.scatter(df_viz, x = "Time", y = "value", title = GRAPH_TITLE, color = "Location", labels={"value": f"{VARIABLE}"})
+fig = px.scatter(df_viz, x = "times", y = "value_mean", title = GRAPH_TITLE, color = "location", labels={"value_mean": f"{VARIABLE}", "times" : "time"})
 # Plot creation
 st.plotly_chart(fig)
 
