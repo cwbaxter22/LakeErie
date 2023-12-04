a12648a9f9cabc166b7d1114fe36b4d3ae654586
Colin Baxter <cwbaxter@uw.edu>
2023-12-03 18:29:38 -0800

    -Made code pylint compliant (disabled unnecessary warnings) -Added docstrings and type suggestions for all functions

--- codebase/src/pages/DataPullTest.py
+++
@@ -1,8 +0,0 @@
-import os
-import pandas as pd
-
-dirname = os.path.dirname(__file__)
-
-beach6hourly = os.path.join(dirname, "data/iChart6_data/processed/Beach6_Buoy/daily_tidy_all_data.csv")
-df = pd.read_csv(beach6hourly)
-print(df.head())
