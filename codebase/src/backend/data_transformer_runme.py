9ab9ba1a0acb575bdddafda2bf5fb64f91dddf4c
Cole Morokhovich <nmorok@uw.edu>
2023-12-05 16:57:53 -0800

    Adding the processed files for all but the largest files

--- codebase/src/backend/data_transformer_runme.py
+++ codebase/src/backend/data_transformer_runme.py
@@ -11,55 +11,83 @@ import os
 # dataTransformer.device_downsample_hour("ichart")
 # dataTransformer.device_downsample_day("ichart")
 
-# old
-#dataTransformer = DataTransformer()
-#dataTransformer.set_path()
-#dataTransformer.set_devices(["3100-iSIC", "Beach2_SDL", "Beach2_Tower_iSIC","Walnut_Creek_iSIC"])
-#dataTransformer.across_parameter_aggregate("3100-iSIC", "old")
-#dataTransformer.device_aggregate("old")
-#dataTransformer.tidy_devices("old")
-#dataTransformer.device_downsample_hour("old")
-#dataTransformer.device_downsample_day("old")
-
-# new
-# dataTransformer = DataTransformer()
-# dataTransformer.set_path("../../data/raw/ichart/pivot")
-# dataTransformer.set_devices(["Beach2_Tower", "Beach2_Buoy", "Beach6_Buoy", "TREC_Tower"])
-# dataTransformer.device_aggregate("ichart")
-# dataTransformer.tidy_devices("ichart")
-# dataTransformer.device_downsample_hour("ichart")
-# dataTransformer.device_downsample_day("ichart")
 
 
-hourly_df = pd.read_csv(r"../../data/raw/old/3100-iSIC/Air_Temperature.csv")
-
-# ## is this doubled up in another function?
-# hourly_df["times"] = pd.to_datetime(hourly_df["times"])
-# parameter = hourly_df.columns[1]
-# hourly_df.rename(columns={parameter: 'value'}, inplace=True)
-# #hourly_df["parameter"] = parameter
-
-# print(hourly_df.head())
+#the name of all the folders in a directory
 
 
+#device = os.listdir(f"../../data/raw/{project}")
+class DataWrangler:
+    def __init__(self) -> None:
+        self.project = [ "new", "old"]
+
+    
+    def downsample(self) -> None:
+        for project in self.project:
+            for device in os.listdir(f"../../data/raw/{project}"):
+                if device == "TREC_Tower_iSIC":
+                    continue
+                if device == "Beach2_Tower_iSIC":
+                    continue
+                for filename in os.listdir(f"../../data/raw/{project}/{device}"):
+                    print(device, filename)
+                    if filename.endswith(".csv"):
+                        df = pd.read_csv(f"../../data/raw/{project}/{device}/{filename}")
+                        df["times"] = pd.to_datetime(df["times"])
+                        parameter = df.columns[1]
+                        df.rename(columns={parameter: 'value'}, inplace=True)
+                        df.set_index("times", inplace=True)
+                        hourly_df = df.groupby(["Units"]).resample('H').agg({"value": ["mean", "std"]})
+                        daily_df = df.groupby(["Units"]).resample('D').agg({"value": ["mean", "std"]})
+                        hourly_df.reset_index(inplace=True)
+                        daily_df.reset_index(inplace=True)
+                        hourly_df.set_index("times", inplace=True)
+                        daily_df.set_index("times", inplace=True)
+                        hourly_df.reset_index(inplace=True)
+                        daily_df.reset_index(inplace=True)
+                        hourly_df.columns = [col[0] if col[1] == '' else f"{col[0]}_{col[1]}" for col in hourly_df.columns]
+                        daily_df.columns = [col[0] if col[1] == '' else f"{col[0]}_{col[1]}" for col in daily_df.columns]
+                        hourly_df["parameter"] = parameter
+                        daily_df["parameter"] = parameter
         
-# #set the times column as the index
-# hourly_df.set_index("times", inplace=True)
+                        #save the data to a csv file
+                        hourly_df.to_csv(f"../../data/processed/{project}/{device}/hourly_{filename}", index = False)
+                        daily_df.to_csv(f"../../data/processed/{project}/{device}/daily_{filename}", index = False)
 
-# #groupby the parameter and units, then resample the data to 1 hour intervals
-# print(1)
-# #hourly_df = hourly_df.groupby(["parameter", "Units"]).resample("H").agg({"value": ["mean", "std"]})
-# #hourly_df = hourly_df.groupby(["Units"]).resample("H").agg({"value": ["mean", "std"]})
-# result = hourly_df.resample('H').agg({"value": ["mean", "std"]})
-# print(result.head())
-# #reset the index
-# hourly_df.reset_index(inplace=True)
+                self.parser(f"../../data/processed/{project}/{device}")
+                    
 
-# #combine multiindex columns into one column
-# hourly_df.columns = [col[0] if col[1] == '' else f"{col[0]}_{col[1]}" for col in hourly_df.columns]
 
-# hourly_df["parameter"] = parameter
-# print(hourly_df.head())
-        
-# #save the data to a csv file
-# hourly_df.to_csv("../../data/processed/old/3100-iSIC/Air_Temperature_hourly.csv", index = False)
+    def parser(self, directory: str ) -> None:
+        df_merged = pd.DataFrame()
+        for filename in os.listdir(directory):
+            if filename.endswith(".csv"):
+                if filename.startswith("hourly"):
+                    print("location1:", filename)
+                    df = pd.read_csv(f"{directory}/{filename}")
+                    df_merged = pd.concat([df_merged, df], ignore_index=True)
+        df_merged.to_csv(f"{directory}/tidy_hourly_all_data.csv", index = False)
+
+        df_merged = pd.DataFrame()
+        for filename in os.listdir(directory):
+            if filename.endswith(".csv"):
+                if filename.startswith("daily"):
+                    print("location2:", filename)
+                    df = pd.read_csv(f"{directory}/{filename}")
+                    df_merged = pd.concat([df_merged, df], ignore_index=True)
+        df_merged.to_csv(f"{directory}/tidy_daily_all_data.csv", index = False)
+        print("tidied the data for ", directory)
+
+
+
+
+
+
+
+
+
+
+
+datawrangler = DataWrangler()
+datawrangler.downsample()
+
