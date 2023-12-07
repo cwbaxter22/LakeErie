f2018a5c0ae44cb92ab7c852ce3be0b4e4cab0c9
Ben Makhlouf <makhlobe@gmail.com>
2023-12-07 12:34:20 -0800

    add scaffolding to test leaflet map production function

--- codebase/test/frontend/test_advanced_stats.py
+++ codebase/test/frontend/test_advanced_stats.py
@@ -47,5 +47,49 @@ class TestCreateAnomlyGraph(unittest.TestCase):
 
 ###############
 
+class TestAnomalydecomp(unittest.TestCase):
+
+    """ Test cases for anomalydecomp function, which provides graphs of statistical decomposition of anomaly calculation, 
+    including __________"""
+
+    def test_valid_data(self)
+        "one shot test which brings in valid data, expecting no errors"
+
+    def nonzero_testvalues(self)
+        "test to make sure iqr and clean_alpha are not specified to 0 values, expecting raisevalueerror if not"
+
+    def test_numeric_in_data(self)
+        "test to make sure data after NA's are removed does not have any non-numeric values, expecting raisevalueerror"
+
+    def testAnomalydf_is_created (self)
+        "test to make sure Anomaly df has a non0 value"
+
+    def plotly_graph_is_created(self)
+        "test to make sure plotly graph is created, expecting raisevalueerror if not"
+
+
+
+
+##############
+
+class Testcreate_leaflet_map(unittest.TestCase):
+
+    """Test cases for create_leaflet_map function, which creates a map of the locations of the stations"""
+
+    def test_valid_data(self)
+        "one shot test which brings in valid data, expecting no errors"
+
+    def test_selected_location(self)
+        "test to make sure the selected location is in the stations list, expecting raisevalueerror if not"
+
+    def test_map_is_created(self)
+        "test to make sure map is created, expecting raisevalueerror if not"
+
+    def test_seven_stations(self)
+        "test to make sure there are 7 stations in the map, expecting raisevalueerror if not"
+
+    
+################
+
 
 
