ee13add1c8fde8c3aff5a98cfc991476248c679f
Cole Morokhovich <nmorok@uw.edu>
2023-12-07 11:48:56 -0800

    Modifying the data_combiner for the correct path and for hourly/daily data.

--- codebase/src/backend/data_combiner.py
+++ codebase/src/backend/data_combiner.py
@@ -8,6 +8,7 @@ import pandas as pd
 
 from config_combine import COMBINE_MAP
 
+# Run for daily data
 for name, all_device in COMBINE_MAP.items():
     combined_df = []
     for prod, prod_device in zip(["ichart", "old", "new"], all_device):
@@ -26,4 +27,29 @@ for name, all_device in COMBINE_MAP.items():
         )
     combined_df = pd.concat(combined_df, ignore_index=True).set_index("times")
     combined_df["location"] = name
-    combined_df.to_csv(f"../../data/processed/combined/{name}.csv")
+    if not os.path.exists(f"../../data/processed/combined/{name}"):
+        os.mkdir(f"../../data/processed/combined/{name}")
+    combined_df.to_csv(f"../../data/processed/combined/{name}/daily.csv")
+
+# Run for hourly data
+for name, all_device in COMBINE_MAP.items():
+    combined_df = []
+    for prod, prod_device in zip(["ichart", "old", "new"], all_device):
+        # Skip in 
+        if prod_device is None:
+            continue
+        if prod == "ichart":
+            path = f"../../data/processed/{prod}/{prod_device}/tidy_hourly_all_data.csv"
+        else:
+            path = f"../../data/processed/{prod}/{prod_device}/tidy_hourly_all_data.csv"
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"Path {path} does not exist.")
+
+        combined_df.append(
+            pd.read_csv(path)
+        )
+    combined_df = pd.concat(combined_df, ignore_index=True).set_index("times")
+    combined_df["location"] = name
+    if not os.path.exists(f"../../data/processed/combined/{name}"):
+        os.mkdir(f"../../data/processed/combined/{name}")
+    combined_df.to_csv(f"../../data/processed/combined/{name}/hourly.csv")
