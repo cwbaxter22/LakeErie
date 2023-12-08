3840cab2feb5c75e21cd5b6d2a61f8b3c9f9a666
Ben Makhlouf <makhlobe@gmail.com>
2023-12-07 18:20:23 -0800

    re added raiseAllertErrors for subsequent testing code

--- codebase/src/frontend/anomaly.py
+++ codebase/src/frontend/anomaly.py
@@ -4,60 +4,46 @@ import pandas as pd
 import plotly.io as pio
 import pytimetk
 
-#################################
-###### FUNCTIONS
-# ############################### 
-
-#######################################################################################################
-####### Function to create dataframe #######
-#######################################################################################################
-
-def df_creation(current_file_dir: str, frequency: str, selected_location: str) -> pd.DataFrame:
-    """
-    Create dataframe using user-selected site.
-
-    Arguments:
-    ----------
-    current_file_dir (str): current file directory
-    frequency (str): frequency of data
-    selected_location (str): user-selected location
-
-    Returns:
-    ----------
-    df_chosen_loc (df): df containing user-selected location data
-    """
-    st.subheader("Select a data location")
-    # Static list of data collection sites
-    # Names of the csv files
-    LOCATIONS_UNFORMATTED = ["Beach2_Buoy", "Beach2_Tower", "Beach6_Buoy", "TREC_Tower"]
-    # Names of locations user can pick from
-    LOCATIONS_DISPLAY = ["Beach 2 Buoy", "Beach 2 Tower", "Beach 6 Buoy", "TREC Tower"]
-    
-    # Allow the user to choose one location from the dropdown menu
-    selected_location = st.selectbox("Choose a data collection site", LOCATIONS_DISPLAY)
-    
-    # Index the array containing all of the available csv names using the selected location
-    location_index = LOCATIONS_DISPLAY.index(selected_location)
-    location_selected_call = LOCATIONS_UNFORMATTED[location_index]
-
-    # List comprehension to create a list of paths to needed dataframes
-    df_dir = current_file_dir + "/data/iChart6_data/processed/" + location_selected_call + frequency
-
-    # Read the selected location's dataframe
-    df_chosen_loc = pd.read_csv(df_dir)
-    return df_chosen_loc
-
 #######################################################################################################
 ##### Function to create the time series trendline 
 #######################################################################################################
 
+import pytimetk
 
 def create_trendline(data):
     """
     Use pytimetk to create a non-linear trendline plot for the selected data
 
-    """
+    Parameters:
+    - data: pandas DataFrame
+        Input data containing a 'times' column and a 'value_mean' column.
 
+    Returns:
+    - fig (plotly figure): figure of the trendline plot
+
+    Raises:
+    - ValueError: If 'data' is not a pandas DataFrame.
+    - ValueError: If 'times' column is not present in 'data' 
+    - ValueError: If 'value_mean' column is not present in 'data' 
+    - ValueError: If 'times' column does not have at least 3 non-0 or non-NA values 
+    - ValueError: If 'times' column is not a datetime object.
+    """
+    
+    if not isinstance(data, pd.DataFrame):
+        raise ValueError("'data' must be a pandas DataFrame.")
+    
+    if 'times' not in data.columns:
+        raise ValueError("The 'times' column is not present in the data.")
+    
+    if 'value_mean' not in data.columns:
+        raise ValueError("The 'value_mean' column is not present in the data.")
+    
+    if data['times'].count() < 3 or data['value_mean'].count() < 3:
+        raise ValueError("Each column must have at least 3 non-0 or non-NA values.")
+    
+    if not pd.api.types.is_datetime64_any_dtype(data['times']):
+        raise ValueError("The 'times' column must be a datetime object.")
+    
     fig = pytimetk.plot_timeseries(
         data=data,
         date_column='times',
@@ -66,13 +52,59 @@ def create_trendline(data):
     
     return fig
 
+
+###########################################
 ### function for anomaly calculation
+###########################################
+
 
 def create_anomaly_graph(data, period=7, iqr_alpha=0.05, clean_alpha=0.75):
     """
     Use pytimetk to plot a chosen variable over a selected time period, with statistical
     analysis to produce anomaly bars and display values considered as anomalies.
-    """
+
+    Parameters:
+    - data: pandas DataFrame
+        Input data containing a 'times' column and a 'value_mean' column.
+    - period: int, optional
+        The time period for analysis. Default is 7.
+    - iqr_alpha: float, optional
+        The alpha level for the IQR-based anomaly detection. Default is 0.05.
+    - clean_alpha: float, optional
+        The alpha level for data cleaning. Default is 0.75.
+
+    Returns:
+    - anom_plot: Depends on the plotting engine used (e.g., plotly figure).
+
+    Raises:
+    - ValueError: If 'data' is not a pandas DataFrame.
+    - ValueError: If 'times' column is not present in 'data'.
+    - ValueError: If 'value_mean' column is not present in 'data'.
+    - ValueError: If 'data' contains NaN values in the 'value_mean' column.
+    - ValueError: If 'period' is not a positive integer.
+    - ValueError: If 'iqr_alpha' is not between 0 and 1.
+    - ValueError: If 'clean_alpha' is not between 0 and 1.
+    - ImportError: If pytimetk is not installed.
+
+"""
+    if not isinstance(data, pd.DataFrame):
+        raise ValueError("'data' must be a pandas DataFrame.")
+    
+    if 'times' not in data.columns:
+        raise ValueError("The 'times' column is not present in the data.")
+    
+    if 'value_mean' not in data.columns:
+        raise ValueError("The 'value_mean' column is not present in the data.")
+    
+    if not isinstance(period, int) or period <= 0:
+        raise ValueError("'period' must be a positive integer.")
+    
+    if not (0 <= iqr_alpha <= 1):
+        raise ValueError("'iqr_alpha' must be between 0 and 1.")
+    
+    if not (0 <= clean_alpha <= 1):
+        raise ValueError("'clean_alpha' must be between 0 and 1.")
+    
 
     data_cleaned = data.dropna(subset=['value_mean'])
 
@@ -86,52 +118,64 @@ def create_anomaly_graph(data, period=7, iqr_alpha=0.05, clean_alpha=0.75):
         clean="min_max"
     )
 
-    anomplot = pytimetk.plot_anomalies(
+    anom_plot = pytimetk.plot_anomalies(
         data=anomalize_df,
         date_column='times',
         engine='plotly',
         title='Plot Anomaly Bands'
     )
 
-    return anomplot
-
-
-#### Function to create the leaflet map 
-
-def create_leaflet_map(selected_location):
-    # Predefined station locations
-    stations = {
-        "TREQ Station": (42.109657, -80.155201),
-        "Bay Bouy": (42.126427, -80.145289),
-        "Beach 2 Tower": (42.151768, -80.132625),
-        "Beach 2 Bouy": (42.152811, -80.137925),
-        "Beach 6 Bouy": (42.157562, -80.131492),
-        "Nearshore Bouy": (42.170685, -80.123547),
-        "Walnut Creek Bouy": (42.103838, -80.256560),
-    }
-
-    latitudes, longitudes = zip(*stations.values())
-    map_center = [sum(latitudes) / len(latitudes), sum(longitudes) / len(longitudes)]
-
-    # Create a Folium map with 'cartodbdark_matter' tile layer
-    m = folium.Map(location=map_center, zoom_start=10, tiles='cartodbdark_matter')
-
-    for station, (lat, lon) in stations.items():
-        icon_color = 'red' if station == selected_location else 'grey'
-        folium.Marker([lat, lon], popup=station, icon=folium.Icon(color=icon_color)).add_to(m)
-
-    return m
+    return anom_plot
 
 
 #########################################################################################################
 ### Function for the statistical decomposition 
 #########################################################################################################
 
-
 def anomaly_decomp(data, period=7, iqr_alpha=0.05, clean_alpha=0.75):
     """
+    Use pytimetk to plot statistical decomposition of anomaly calculation.
+
+    Parameters:
+    - data: pandas DataFrame
+        Input data containing a 'times' column and a 'value_mean' column.
+    - period: int, optional
+        The time period for analysis. Default is 7.
+    - iqr_alpha: float, optional
+        The alpha level for the IQR-based anomaly detection. Default is 0.05.
+    - clean_alpha: float, optional
+        The alpha level for data cleaning. Default is 0.75.
+
+    Returns:
+    - decomp: figure with statistical decomposition of anomaly calculation
+
+    Raises:
+    - ValueError: If 'data' is not a pandas DataFrame.
+    - ValueError: If 'times' column is not present in 'data'.
+    - ValueError: If 'value_mean' column is not present in 'data'.
+    - ValueError: If 'period' is not a positive integer.
+    - ValueError: If 'iqr_alpha' is not between 0 and 1.
+    - ValueError: If 'clean_alpha' is not between 0 and 1.
     """
-   
+    
+    if not isinstance(data, pd.DataFrame):
+        raise ValueError("'data' must be a pandas DataFrame.")
+    
+    if 'times' not in data.columns:
+        raise ValueError("The 'times' column is not present in the data.")
+    
+    if 'value_mean' not in data.columns:
+        raise ValueError("The 'value_mean' column is not present in the data.")
+    
+    if not isinstance(period, int) or period <= 0:
+        raise ValueError("'period' must be a positive integer.")
+    
+    if not (0 <= iqr_alpha <= 1):
+        raise ValueError("'iqr_alpha' must be between 0 and 1.")
+    
+    if not (0 <= clean_alpha <= 1):
+        raise ValueError("'clean_alpha' must be between 0 and 1.")
+    
     data_cleaned = data.dropna(subset=['value_mean'])
 
     anomalize_df = pytimetk.anomalize(
@@ -152,3 +196,5 @@ def anomaly_decomp(data, period=7, iqr_alpha=0.05, clean_alpha=0.75):
     )
 
     return decomp
+
+#######
